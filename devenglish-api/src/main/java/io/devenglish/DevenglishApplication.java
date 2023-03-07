ac3a98e8d1965dfe468b0d98972c6b94eb27e7cd
= <crysthian.melo@aluno.ifsp.edu.br>
2023-03-06 23:24:39 -0300

    Ajustando configs iniciais da api

---
+++ devenglish-api/src/main/java/io/devenglish/DevenglishApplication.java
@@ -0,0 +1,13 @@
+package io.devenglish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevenglishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevenglishApplication.class, args);
+	}
+
+}
